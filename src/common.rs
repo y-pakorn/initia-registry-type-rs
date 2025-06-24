d95dbbdf84d9e488bb6f2e97f8e85b4880f178d6
Pakorn Nathong <pakorn.nth@hotmail.com>
2025-06-24 15:24:20 +0700

    feat: add common getter

--- src/common.rs
+++ src/common.rs
@@ -7,3 +7,11 @@ pub enum ImageType {
     #[serde(rename = "svg")]
     SVG(String),
 }
+
+impl ImageType {
+    pub fn url(&self) -> &str {
+        match self {
+            ImageType::PNG(url) | ImageType::SVG(url) => url,
+        }
+    }
+}
